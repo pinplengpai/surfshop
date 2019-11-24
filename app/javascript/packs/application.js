56fe2f5c322761a4ef5e154f79a22183e01c2126
Plengpai Ratnajarn <pinplengpai@gmail.com>
2019-11-24 12:24:01 -0500

    finished the homepage and changed fonts

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,9 +1,10 @@
-import { fixedForm } from '../plugins/init_fix_form';
-fixedForm();
-import "bootstrap";
-import "../plugins/flatpickr";
-import "../plugins/init_autocomplete";
-import { initUpdateNavbarOnScroll } from '../components/navbar';
-initUpdateNavbarOnScroll();
+// import { fixedForm } from '../plugins/init_fix_form';
+// fixedForm();
+// import "bootstrap";
+// import "../plugins/flatpickr";
+// import "../plugins/init_autocomplete";
+// import { initUpdateNavbarOnScroll } from '../components/navbar';
+// initUpdateNavbarOnScroll();
 import { loadDynamicBannerText } from '../components/banner';
 loadDynamicBannerText();
+
