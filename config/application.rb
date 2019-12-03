cac92cba19a7bab3a9f467e95cb5f13f4dc4f9e3
Plengpai Ratnajarn <pinplengpai@gmail.com>
2019-12-03 12:40:14 -0500

    add  config.assets.precompile << /\.(?:svg|eot|woff|ttf)$/ in application.rb

--- config/application.rb
+++ config/application.rb
@@ -12,6 +12,7 @@ module Surfshop
     config.load_defaults 5.2
     config.assets.initialize_on_precompile = false
     config.assets.paths << Rails.root.join("app", "assets", "fonts")
+    config.assets.precompile << /\.(?:svg|eot|woff|ttf)$/
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
